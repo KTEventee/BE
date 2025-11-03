18d7bc7bb322b5fcb1f4f5db5da45d1657933bdb
hyeonda02 <alceste951005@naver.com>
2025-11-03 16:58:00 +0900

    Initial project

---
+++ src/main/java/com/server/eventee/EventeeApplication.java
@@ -0,0 +1,13 @@
+package com.server.eventee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventeeApplication.class, args);
+	}
+
+}
