d8af58e7ad870a160db11c29b7cdbe19b6ca085a
yongsik <wkdrndydtlr@naver.com>
2025-11-05 09:25:40 +0900

    (feat) BaseResponse환경 구성, JWT코드 설정, 시큐리티 기본 설정 등 기본적인 환경 구성

---
+++ src/main/java/com/server/eventee/global/token/vo/TokenResponse.java
@@ -0,0 +1,10 @@
+package com.server.eventee.global.token.vo;
+
+public record TokenResponse(
+        AccessToken accessToken,
+        RefreshToken refreshToken
+) {
+    public static TokenResponse of(AccessToken accessToken, RefreshToken refreshToken) {
+        return new TokenResponse(accessToken, refreshToken);
+    }
+}
