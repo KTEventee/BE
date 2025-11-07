b18b3320115478f12bea369ccede698323b32e7d
hyeonda02 <alceste951005@naver.com>
2025-11-07 17:56:19 +0900

    (feat) 닉네임 중복 확인 api 구현

--- src/main/java/com/server/eventee/domain/member/service/MemberService.java
+++ src/main/java/com/server/eventee/domain/member/service/MemberService.java
@@ -1,5 +1,9 @@
 package com.server.eventee.domain.member.service;
 
+import com.server.eventee.domain.member.model.Member;
+import jakarta.validation.constraints.NotBlank;
+
 public interface MemberService {
 
+  boolean confirmNickName(Member member, String nickname);
 }
