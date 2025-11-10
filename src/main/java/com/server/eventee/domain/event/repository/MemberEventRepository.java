d01bd495b81e4f1880601f02fc1986b858de8ea4
hyeonda02 <alceste951005@naver.com>
2025-11-11 08:51:28 +0900

    (feat) 이벤트 생성 api 추가

--- src/main/java/com/server/eventee/domain/event/repository/MemberEventRepository.java
+++ src/main/java/com/server/eventee/domain/event/repository/MemberEventRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MemberEventRepository extends JpaRepository<MemberEvent, Long> {
@@ -16,4 +17,6 @@ public interface MemberEventRepository extends JpaRepository<MemberEvent, Long> 
   List<MemberEvent> findAllByEventAndIsDeletedFalse(Event event);
 
   boolean existsByMemberAndEventAndIsDeletedFalse(Member member, Event event);
+
+  Optional<MemberEvent> findByMemberAndEventAndIsDeletedFalse(Member member, Event event);
 }
--- src/main/java/com/server/eventee/domain/group/repository/GroupRepository.java
+++ src/main/java/com/server/eventee/domain/group/repository/GroupRepository.java
@@ -1,11 +1,14 @@
 package com.server.eventee.domain.group.repository;
 
 
+import com.server.eventee.domain.event.model.Event;
 import com.server.eventee.domain.group.model.Group;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface GroupRepository extends JpaRepository<Group,Long> {
     Optional<Group> findGroupByGroupId(Long groupId);
+    List<Group> findAllByEventAndIsDeletedFalse(Event event);
 }
