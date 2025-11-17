be6ff38e2141a4754062d49f463d48f59829ebe2
hyeonda02 <alceste951005@naver.com>
2025-11-17 17:48:43 +0900

    (feat) CORS 설정 추가 및 server-url 추가

--- src/main/java/com/server/eventee/global/config/CorsConfig.java
+++ src/main/java/com/server/eventee/global/config/CorsConfig.java
@@ -1,5 +1,33 @@
 package com.server.eventee.global.config;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
 public class CorsConfig {
 
+  @Value("${cors.allowed-origins}")
+  private String[] allowedOrigins;
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+    configuration.addAllowedHeader("*");
+    configuration.addAllowedMethod("*");
+    configuration.setAllowCredentials(true);
+    configuration.addExposedHeader("Authorization");
+    configuration.setMaxAge(3600L);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
+  }
 }
