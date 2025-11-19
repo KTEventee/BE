5dbc1a3b0af661207fea14db477227cba29e7e4c
hyeonda02 <alceste951005@naver.com>
2025-11-19 12:47:54 +0900

    refactor: security config 경로 추가

--- src/main/java/com/server/eventee/global/config/SecurityConfig.java
+++ src/main/java/com/server/eventee/global/config/SecurityConfig.java
@@ -44,7 +44,8 @@ public class SecurityConfig {
                 .requestMatchers(
                     "/v3/**", "/swagger-ui.html", "/swagger-ui/**",
                     "/swagger-resources/**", "/api-test/**",
-                    "/api/v1/auth/**", "/actuator/**"
+                    "/api/v1/auth/**", "/actuator/**",
+                    "https://api.eventee.cloud/api/v1/auth/google/**"
                 ).permitAll()
                 .anyRequest().authenticated()
             )
